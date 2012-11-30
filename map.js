5de565c7f77d8468c9d990b78124d8061d0ca3e3
Kai Hendry <hendry@webconverger.com>
2012-11-30 13:05:24 +0800

    Use textContent instead of innerText http://stackoverflow.com/a/1359507

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ var iOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true: false);
 c = document.getElementById("where");
 
 if (iOS) {
-	c.innerHTML = '<a href="maps://?q=' + c.innerText + '">' + c.innerText + '</a>';
+	c.innerHTML = '<a href="maps://?q=' + c.textContent + '">' + c.textContent + '</a>';
 } else {
-	c.innerHTML = '<a href="http://gothere.sg/maps#q:' + c.innerText.replace(",", "%2C") + '">' + c.innerText + '</a>';
+	c.innerHTML = '<a href="http://gothere.sg/maps#q:' + c.textContent.replace(",", "%2C") + '">' + c.textContent + '</a>';
 }
