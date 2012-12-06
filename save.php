6dbcc5132cfe0ddbb664b1e3e1460d21833cd2ea
Kai Hendry <hendry@webconverger.com>
2012-12-06 13:16:17 +0800

    no more meetup, keep it suckless

---
+++ save.php
@@ -0,0 +1,12 @@
+<?php
+
+if (empty($_POST["email"])) {
+	die();
+}
+
+$fp = fopen('.emails.txt', 'a');
+fwrite($fp, date("c") . " " . $_SERVER["REMOTE_ADDR"] . " " . $_POST["email"] . "\n");
+fclose($fp);
+?>
+
+<h1>Hey, <?php echo $_POST["email"]; ?>, you will be notified of our next event.</h1>
