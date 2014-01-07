005d797775dae1e44be54ff46f211786265b3029
Kai Hendry <hendry@webconverger.com>
2014-01-07 14:30:44 +0800

    gen ics script

--- ics/genics.sh
+++ ics/genics.sh
@@ -1,12 +1,21 @@
+#!/bin/bash
+if ! test "$1" || test $(date +%Y%m%d) -gt "$1"
+then
+	echo Missing \$ $0 $(date --date='2 week' +%Y%m%d) \# argument
+	exit 1
+fi
+
+cat <<EOF
 BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Hack and Tell//NONSGML v1.0//EN
 BEGIN:VEVENT
-UID: 1389076210723542885
+UID: $(date +%s%N)
 SUMMARY: Hack and Tell
 LOCATION: Hackerspace.SG, 344B King George's Avenue
 DESCRIPTION: Show and tell for hackers. Five minute demos, each followed by five minutes of questions.
-DTSTART;TZID=Asia/Singapore: 20140128T200000
-DTEND;TZID=Asia/Singapore: 20140128T220000
+DTSTART;TZID=Asia/Singapore: ${1}T200000
+DTEND;TZID=Asia/Singapore: ${1}T220000
 END:VEVENT
 END:VCALENDAR
+EOF
