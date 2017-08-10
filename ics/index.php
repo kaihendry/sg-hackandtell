d08b12e3c60ca2c8f5b14f69ebbaf39b0f7a839a
Kai Hendry <hendry@webconverger.com>
2017-08-11 03:49:25 +0800

    Hopefully my last commit

--- ics/index.php
+++ ics/index.php
@@ -2,7 +2,7 @@
 
 date_default_timezone_set('Asia/Singapore');
 $fp = fopen('debug.log', 'a');
-fwrite($fp, date("c") . " " . gethostbyaddr($_SERVER['REMOTE_ADDR']) . " " . $_SERVER['HTTP_USER_AGENT'] . "\n");
+@fwrite($fp, date("c") . " " . gethostbyaddr($_SERVER['REMOTE_ADDR']) . " " . $_SERVER['HTTP_USER_AGENT'] . "\n");
 fclose($fp);
 
 header('Content-type: text/calendar');
